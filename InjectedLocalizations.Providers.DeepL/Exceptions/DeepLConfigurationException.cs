97baa7f6ae8c4759202449b0004bd762e98cccb6
Pablo Jimena <hossmi@gmail.com>
2023-03-23 19:22:29 +0100

    Add DeepLConfigurationException

---
+++ InjectedLocalizations.Providers.DeepL/Exceptions/DeepLConfigurationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace InjectedLocalizations.Exceptions
+{
+    [Serializable]
+    public class DeepLConfigurationException : ArgumentException
+    {
+        public DeepLConfigurationException(string paramName)
+            : base($"Missing DeepL configuration parameter '{paramName}'", paramName) { }
+
+        protected DeepLConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
