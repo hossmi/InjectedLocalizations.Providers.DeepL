50208f49f8b80861d88ffa6e7f4c30b7fb8f480c
Pablo Jimena <hossmi@gmail.com>
2023-03-26 20:42:59 +0200

    Add DeepLApiException for to check DeepL Translator instance

---
+++ InjectedLocalizations.Providers.DeepL/Exceptions/DeepLApiException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+using DeepL;
+
+namespace InjectedLocalizations.Exceptions
+{
+    [Serializable]
+    public class DeepLApiException : LocalizationException
+    {
+        public DeepLApiException() : base($"Cannot get {typeof(Translator)} instance.")
+        {
+        }
+
+        protected DeepLApiException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
