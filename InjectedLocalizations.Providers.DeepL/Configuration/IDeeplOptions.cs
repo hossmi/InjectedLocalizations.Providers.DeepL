ceeda4733317c8b423c9d7f96bcffe05d16e83fc
Pablo Jimena <hossmi@gmail.com>
2023-03-26 20:46:21 +0200

    Add IDeeplOptions CultureMap property

--- InjectedLocalizations.Providers.DeepL/Configuration/IDeeplOptions.cs
+++ InjectedLocalizations.Providers.DeepL/Configuration/IDeeplOptions.cs
@@ -1,8 +1,12 @@
-﻿namespace InjectedLocalizations.Configuration
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace InjectedLocalizations.Configuration
 {
     public interface IDeeplOptions
     {
         string ApiKey { get; set; }
         string Url { get; set; }
+        IReadOnlyDictionary<CultureInfo, string> CultureMap { get; set; }
     }
 }
