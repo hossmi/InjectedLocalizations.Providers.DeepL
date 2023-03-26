2fff436305d1422321bd1bca523d756cbab1ab08
Pablo Jimena <hossmi@gmail.com>
2023-03-26 20:43:25 +0200

    Add DefaultDeepLCultureMap

---
+++ InjectedLocalizations.Providers.DeepL/Configuration/DefaultDeepLCultureMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace InjectedLocalizations.Configuration
+{
+    public class DefaultDeepLCultureMap : IDeeplCultureMap
+    {
+        private readonly IReadOnlyDictionary<CultureInfo, string> map;
+
+        public DefaultDeepLCultureMap(IReadOnlyDictionary<CultureInfo, string> map)
+        {
+            this.map = map;
+        }
+
+        public string MapToDeepl(CultureInfo culture)
+        {
+            if (this.map.TryGetValue(culture, out string map))
+                return map;
+
+            return culture.TwoLetterISOLanguageName;
+        }
+    }
+}
