0c77aa52d84f868e18bea637d788e73d4cf60af8
Pablo Jimena <hossmi@gmail.com>
2023-03-29 20:15:51 +0200

    Add tests

---
+++ InjectedLocalizations.Providers.DeepL.Tests/Models/FakeParsedMember.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using InjectedLocalizations.MemberParsing;
+
+namespace InjectedLocalizations.Models
+{
+    public class FakeParsedMember : IParsedMember
+    {
+        private readonly IReadOnlyList<IToken> tokens;
+
+        public FakeParsedMember(MemberInfo member, params IToken[] tokens)
+        {
+            this.Member = member;
+            this.tokens = tokens.ToList();
+        }
+
+        public MemberInfo Member { get; }
+        public int Count => this.tokens.Count;
+
+        public static IParsedMember BuildFor<T>(string memberName, params IToken[] tokens)
+        {
+            MemberInfo member;
+
+            member = typeof(T)
+                .GetMember(memberName)
+                .Single();
+
+            return new FakeParsedMember(member, tokens);
+        }
+
+        public IEnumerator<IToken> GetEnumerator() => this.tokens.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ InjectedLocalizations.Providers.DeepL.Tests/Models/ISampleInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace InjectedLocalizations.Models
+{
+    public interface ISampleInterface
+    {
+        string The_file_already_exists { get; }
+        string There_are_0_apples(int count);
+    }
+}
