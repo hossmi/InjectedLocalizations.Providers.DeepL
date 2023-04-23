fa1c5cda9bb4c7a2912ace252bb631211cf355b9
hossmi <hossmi@gmail.com>
2023-04-23 20:12:12 +0200

    Add nuget reference to InjectedLocalizations for to make integration tests

---
+++ InjectedLocalizations.Providers.DeepL.Tests/Models/FakeCurrentCultureProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using InjectedLocalizations.Providers;
+
+namespace InjectedLocalizations.Models
+{
+    public class FakeCurrentCultureProvider : ICurrentCultureProvider
+    {
+        public CultureInfo CurrentCulture { get; set; }
+
+        public bool TryGetCurrent(out CultureInfo culture)
+        {
+            culture = this.CurrentCulture;
+            return culture != null;
+        }
+    }
+}
